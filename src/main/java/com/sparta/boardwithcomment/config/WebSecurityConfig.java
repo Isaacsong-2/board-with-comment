51308c7a4cd623d6f2a02ed40cc72994b5cfef64
SongIsak <thddltkr10@naver.com>
2023-06-27 15:26:22 +0900

    get메소드로 조회시 권한허용

--- src/main/java/com/sparta/boardwithcomment/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/boardwithcomment/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import com.sparta.boardwithcomment.security.AuthenticationSuccessHandlerImpl;
 import com.sparta.boardwithcomment.security.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -66,15 +67,10 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers("/api/auth/**").permitAll() // '/api/auth/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers(HttpMethod.GET, "/api/posts").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/posts/{id}").permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
-
-//        http.formLogin((formLogin) ->
-//                formLogin
-//                        .loginPage("/api/auth/login")
-//                        .successHandler(authenticationSuccessHandler())
-//                        .permitAll()
-//        );
 
         // 필터 관리
         http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
